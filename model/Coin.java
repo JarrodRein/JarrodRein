28a036f2b2faaf57b21832d74687440d4583025a
Jarrod Rein <jrein@uco.edu>
2021-08-31 15:25:25 -0500

    Final Commit

--- model/Coin.java
+++ model/Coin.java
@@ -1,7 +1,9 @@
 package model;
 
 public class Coin {
-    
+    public static final String NICKEL = "Nickel";
+    public static final String DIME = "Dime";
+    public static final String QUARTER = "Quarter";
     private int value;
 
     public Coin(int value){
@@ -10,5 +12,16 @@ public class Coin {
 
     public int getValue(){
         return value;
+    }
+    @Override
+    public String toString(){
+        String name = "N/A";
+        switch (value){
+            case 5: name = Coin.NICKEL; break;
+            case 10: name = Coin.DIME; break;
+            case 25: name = Coin.QUARTER; break;
+        }
+        return name;
+        
     }
 }
